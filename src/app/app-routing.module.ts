2d7129fe6608508fef1676974492100841769c51
Francisco Larrea <flarreas@gmail.com>
2023-01-17 16:25:21 -0300

    #02 beautyfruit fix app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,6 @@ const routes: Routes = [
     loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
   },
   {
-    path: 'cartview',
-    loadChildren: () => import('./pages/cartview/cartview.module').then( m => m.CartviewPageModule)
-  },
-  {
-    path: 'payments',
-    loadChildren: () => import('./pages/payments/payments.module').then( m => m.PaymentsPageModule)
-  },
-  {
     path: 'cancel',
     loadChildren: () => import('./pages/cancel/cancel.module').then( m => m.CancelPageModule)
   },
