060af736371ce1976ad405c6ff7723754e41e31f
Francisco Larrea <flarreas@gmail.com>
2023-01-18 10:51:30 -0300

    #02 beautyfruit Preparing cancel and success pages

--- src/app/pages/cancel/cancel.page.ts
+++ src/app/pages/cancel/cancel.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cancel',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CancelPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  getBackToBuy(){
+    this.router.navigate(['/tabs/products']);
+  }
+
 }
+
--- src/app/pages/success/success.page.ts
+++ src/app/pages/success/success.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-success',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuccessPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  backToBuy(){
+    this.router.navigate(['/tabs/products']);
+  }
+
 }
+
